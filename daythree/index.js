e0ac6d4be0d25d36647315ad0d32e8a0b2cf9fd4
asadbahi <possiblepossible51@gmail.com>
2024-07-16 11:53:59 +0500

    day three

--- daythree/index.js
+++ daythree/index.js
@@ -120,4 +120,24 @@ break;
 
  }
 
- 
+//  task 5
+
+
+let grade=96
+switch(true) {
+   case grade >= 90 : 
+   console.log("A")
+   break;
+   case grade >= 80 :
+     console.log("B")
+   break;
+   case grade >= 60: 
+   console.log("C")
+   break;
+   case grade >= 40 : 
+   console.log("D")
+   break;
+   case grade < 40 :
+     console.log("Fail")
+   break;
+}
