c1d43686b3e097339b72aa0e1f61ee3e56c756a1
asadbahi <possiblepossible51@gmail.com>
2024-07-17 10:24:05 +0500

    day five start

---
+++ day5/index.js
@@ -0,0 +1,22 @@
+// Task 1
+function evenorodd(num) {
+        if(num % 2 ==0){
+            console.log("Its a Even Number");
+        }else{
+            
+            console.log("Its a Odd Number");
+        }
+}
+
+
+
+evenorodd(53)
+
+
+//TAsk 2
+
+function SquareNumber(num){
+    console.log(num * num);
+return num * num
+}
+SquareNumber(5)
